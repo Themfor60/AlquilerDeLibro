d4fe25d91d31296e293cadb12377e759bc5e1552
Uriel Aquino <143182625+Themfor60@users.noreply.github.com>
2025-03-12 21:18:34 -0300

    Update Usuario.cs

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-
-namespace AlquilerDeLibro.Models;
-
 public partial class Usuario
 {
     [Key]
     public int id_usuario { get; set; }
-    [Required (ErrorMessage ="El Nombre es obligatorio") ] 
+    [Required (ErrorMessage ="El Nombre es obligatorio") ]
+
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El nombre solo puede contener letras y espacios.")]
     public string Nombre { get; set; } = null!;
 
-
+    [EmailAddress(ErrorMessage = "El correo no es válido.")]
     [Required(ErrorMessage = "El Correo es obligatorio")]
     public string Correo { get; set; } = null!;
 
+
+    [MinLength(8, ErrorMessage = "La contraseña debe tener al menos 8 caracteres.")]
+    [RegularExpression(@"^[a-zA-Z0-9]*$", ErrorMessage = "La contraseña no debe contener signos especiales.")]
     [Required(ErrorMessage = "La contraseña es obligatorio")]
     public string Contrasena { get; set; } = null!;
 
